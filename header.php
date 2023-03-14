6b2bc64ba94d1de01ea1bc176f6f492bc2614263
v-philip <102297752+v-philip@users.noreply.github.com>
2023-03-14 22:45:29 +0000

    fourth

--- header.php
+++ header.php
@@ -22,7 +22,7 @@ session_start();
     
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
       <div class="container">
-        <a class="navbar-brand" href="index.php">Site Title</a>
+        <a class="navbar-brand" >Site Title</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
